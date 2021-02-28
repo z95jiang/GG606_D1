bdf6f291d9fee2c18b1db096e8764720daa5de81
z95jiang <60328010+z95jiang@users.noreply.github.com>
2021-02-28 09:48:15 -0500

    Update 01_read.R

--- 01_read.R
+++ 01_read.R
@@ -3,3 +3,17 @@
 # Zijian Jiang
 
 # Use this file for reading in the raw data
+##############################################################################
+## GG606 Deliverable 1 ##
+## Tuesday, Feb 23rd	 ##
+## Zijian Jiang        ##
+##############################################################################
+library(ggplot2)
+library(plotrix)
+library(readxl)
+#Read dataset
+ncdb <- read_excel('D://gg606//NCDB_2017.xlsx',sheet = 1) #change the directory to your own, the table is Excel worksheet file instead of CSV, change functionif necessary
+#Exploring dataset
+head(ncdb)
+tail(ncdb)
+names(ncdb)
